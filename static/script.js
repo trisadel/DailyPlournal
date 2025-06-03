00ec9d1a58428f8029cd56fe0d7fef15efaff444
Liyana Damia <liyana.damia114@gmail.com>
2025-06-03 13:15:01 +0800

    edit quotes error

--- static/script.js
+++ static/script.js
@@ -4,10 +4,10 @@ const quotes = [
     { text: "Dream it. Wish it. Do it.", author: "Unknown" },
     { text: "Don’t wait for opportunity. Create it.", author: "Unknown" },
     { text: "Great things never come from comfort zones.", author: "Unknown" },
-    { text: "People leave, memories stay.", author: "Unknown" }
-    { text: "Change is scary, but change is growth.", author: "Unknown" }
-    { text: "Be with someone who makes you happy.", author: "Unknown" }
-    { text: "Go after dreams not people.", author: "Unknown" }
+    { text: "People leave, memories stay.", author: "Unknown" },
+    { text: "Change is scary, but change is growth.", author: "Unknown" },
+    { text: "Be with someone who makes you happy.", author: "Unknown" },
+    { text: "Go after dreams not people.", author: "Unknown" },
     { text: "Happiness is not about getting all you want, It is about enjoying all you have.", author: "Unknown" }
   ];
   
